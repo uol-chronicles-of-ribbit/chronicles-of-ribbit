dac377048c768d00ba26dccbc188735e5ac5b289
Joseph Peterson <jscpeterson@gmail.com>
2020-04-04 14:00:22 -0600

    Fix border generation. Exit tiles are hardcoded

--- main.py
+++ main.py
@@ -23,6 +23,8 @@ class Game:
             # Create first room
             if self.room is None:
                 self.room = Room()
+                self.player.warp(self.room.get_player_spawn_point())
+                self.room.complete_room()  # TODO Only complete when the player earns it!
 
             for event in pygame.event.get():
                 # exit game
@@ -44,7 +46,8 @@ class Game:
 
 
     def update_objects(self):
-        print("nothing updated yet")
+        # print("nothing updated yet")
+        pass
 
     def draw(self):
         self.screen.fill(const.BG_COLOUR)
--- room.py
+++ room.py
@@ -1,7 +1,7 @@
 import random
 
 from Constants import Constants as const
-from obstacle import Obstacle
+from obstacle import Obstacle, Wall
 
 
 class Room:
@@ -20,6 +20,8 @@ class Room:
         self.entrance = entrance
         self.exit = self.create_exit()
         self.obstacles = list()
+        self.walls = list()
+        self.exits = list()
         self.enemies = list()
         self.create_border_wall()
 
@@ -28,19 +30,27 @@ class Room:
         for north_wall in range(0, const.SCREEN_W, const.TILE_SIZE):
             x = north_wall
             y = 0
-            self.obstacles.append(Obstacle(x, y, const.WALL_IMAGE))
-        for east_wall in range(const.SCREEN_H, 0, -const.TILE_SIZE):
+            print(str(x) + ' ' + str(y))
+            self.walls.append(Wall(x, y, const.WALL_IMAGE))
+        for east_wall in range(0, const.SCREEN_H-const.TILE_SIZE, const.TILE_SIZE):
             x = const.SCREEN_W - const.TILE_SIZE
             y = east_wall
-            self.obstacles.append(Obstacle(x, y, const.WALL_IMAGE))
-        for south_wall in range(const.SCREEN_W, 0, -const.TILE_SIZE):
+            print(str(x) + ' ' + str(y))
+            self.walls.append(Wall(x, y, const.WALL_IMAGE))
+        for south_wall in range(const.SCREEN_W-const.TILE_SIZE, 0, -const.TILE_SIZE):
             x = south_wall
             y = const.SCREEN_H - const.TILE_SIZE
-            self.obstacles.append(Obstacle(x, y, const.WALL_IMAGE))
-        for west_wall in range(0, const.SCREEN_H, const.TILE_SIZE):
+            print(str(x) + ' ' + str(y))
+            self.walls.append(Wall(x, y, const.WALL_IMAGE))
+        for west_wall in range(const.SCREEN_H-const.TILE_SIZE, 0, -const.TILE_SIZE):
             x = 0
             y = west_wall
-            self.obstacles.append(Obstacle(x, y, const.WALL_IMAGE))
+            print(str(x) + ' ' + str(y))
+            self.walls.append(Wall(x, y, const.WALL_IMAGE))
+
+        for i, wall in enumerate(self.walls):
+            print(str(i) + ': ' + str(wall.x) + ', ' + str(wall.y))
+        print(len(self.walls))
 
     def create_exit(self):
         """Creates an exit direction randomly from N, E, S, W, excluding entrance direction if it exists."""
@@ -49,15 +59,30 @@ class Room:
             valid_directions.remove(self.entrance)
         return random.choice(valid_directions)
 
-    def open_exit(self):
-        # TODO Remove obstacles at direction point
-        pass
+    def reveal_exit(self):
+        """Reveals the exit tiles and saves them in self.exits"""
+        # midpoint = int((len(self.walls)/8) * (self.exit*2))
+        #
+        # self.exits = self.walls[midpoint-2:midpoint+2]
+
+        # TODO Move hardcoded values or generate dynamically based on width/height
+        if self.exit == self.NORTH:
+            self.exits = self.walls[19:23]
+        elif self.exit == self.EAST:
+            self.exits = self.walls[57:61]
+        elif self.exit == self.SOUTH:
+            self.exits = self.walls[97:101]
+        elif self.exit == self.WEST:
+            self.exits = self.walls[137:141]
+
+        for exit_tile in self.exits:
+            self.walls.remove(exit_tile)
 
     def complete_room(self):
         """Flags the room as complete and opens the exit"""
         assert not self.complete, 'Trying to complete an already complete room!'
         self.complete = True
-        self.open_exit()
+        self.reveal_exit()
 
     def get_player_spawn_point(self):
         """Returns a tuple with the X, Y coordinates of where the player will spawn in the room."""
@@ -67,5 +92,5 @@ class Room:
         return self.PLAYER_SPAWN_POINTS[self.entrance]
 
     def draw(self, screen):
-        for obstacle in self.obstacles:
-            obstacle.draw(screen)
+        for wall in self.walls:
+            wall.draw(screen)
