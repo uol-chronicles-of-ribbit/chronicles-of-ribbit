db634469b010d7c33ae820dae5b18daa086179a3
Tomas Stubbs <tomas.stubbs@gmail.com>
2020-04-04 07:59:19 -0700

    adds movement to character

--- character.py
+++ character.py
@@ -3,9 +3,10 @@ from Constants import Constants as const
 
 
 class Character:
-    def __init__(self, x=const.SCREEN_W//2, y=const.SCREEN_H//2):
+    def __init__(self, x, y, speed):
         self.x = x
         self.y = y
+        self.speed = speed
 
         # TO be defined by child class
         self.img = None
@@ -21,4 +22,16 @@ class Character:
             screen.blit(self.img, (self.x, self.y))
         else:
             pygame.draw.rect(screen, (255,0,0), (self.x, self.y, self.width, self.height), 0)
+
+    def move_left(self):
+        self.x = max(self.x - self.speed, 0)
+
+    def move_right(self):
+        self.x = min(self.x + self.speed, const.SCREEN_W - self.width)
+
+    def move_up(self):
+        self.y = max(self.y - self.speed, 0)
+
+    def move_down(self):
+        self.y = min(self.y + self.speed, const.SCREEN_H - self.height)
 
