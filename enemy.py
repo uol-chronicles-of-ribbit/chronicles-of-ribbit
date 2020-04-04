ce9fad3fecb74d72163945a3388a689279456e5b
Tomas Stubbs <tomas.stubbs@gmail.com>
2020-04-04 12:54:50 -0700

    random enemy moves

--- enemy.py
+++ enemy.py
@@ -2,8 +2,9 @@ import pygame
 from Constants import Constants as const
 from Sprite import Sprite
 from character import Character
-from random import choice, randint
+from random import randint
 
+ENEMY_RANDOM_CHANGE_TICKS = 30
 
 # basics of jumping is commented out for now unless we add later
 class Enemy(Character):
@@ -16,21 +17,25 @@ class Enemy(Character):
                                 [pygame.image.load("images/Fox-idle-right.png"), pygame.image.load("images/Fox-idle-right.png")],     #right
                                 [pygame.image.load("images/Fox-idle-front.png"), pygame.image.load("images/Fox-idle-front.png")],     #up
                                 [pygame.image.load("images/Fox-idle-front.png"), pygame.image.load("images/Fox-idle-front.png")]))    #down
-        self.state = -1 # 1: moving right, -1 moving left
-
+        self.state = randint(0, 3)
+        self.move_tick = 0
 
     def move(self):
-        # check if on border
-        if self.x <= const.TILE_SIZE:
-            self.state = 1
-        elif self.x >= const.SCREEN_H - const.TILE_SIZE - self.height:
-            self.state = -1
-
-        # move in direction
-        if self.state == 1:
+        # attempt movement first
+        if self.state == 0:
+            self.move_left()
+        elif self.state == 1:
+            self.move_up()
+        elif self.state == 2:
             self.move_right()
         else:
-            self.move_left()
+            self.move_down()
 
-        # adding random movement for testing
-        choice([self.move_up, self.move_down])()
+        # check if on border, reverse movement
+        if self.x <= const.TILE_SIZE or self.x >= const.SCREEN_W - const.TILE_SIZE - self.width or \
+                self.y <= const.TILE_SIZE or self.y >= const.SCREEN_H - const.TILE_SIZE - self.height:
+            self.state = (self.state + 2) % 4
+
+        self.move_tick += 1
+        if self.move_tick % ENEMY_RANDOM_CHANGE_TICKS == 0:
+            self.state = randint(0, 3)
