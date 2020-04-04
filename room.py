9e0c065c9317fef7d2fbc1a6eb5f0c7ce163448f
Joseph Peterson <jscpeterson@gmail.com>
2020-04-04 10:54:20 -0600

    Obstacles are stored in room and drawn in draw function

--- room.py
+++ room.py
@@ -19,25 +19,28 @@ class Room:
         self.complete = complete
         self.entrance = entrance
         self.exit = self.create_exit()
+        self.obstacles = list()
+        self.enemies = list()
+        self.create_border_wall()
 
-    def create_border_wall(self, screen):
-        """Draws a border of Obstacle objects, clockwise, surrounding the grid"""
+    def create_border_wall(self):
+        """Creates a border of Obstacle objects, clockwise, surrounding the grid."""
         for north_wall in range(0, const.SCREEN_W, const.TILE_SIZE):
             x = north_wall
             y = 0
-            Obstacle(x, y, const.WALL_IMAGE).draw(screen)
+            self.obstacles.append(Obstacle(x, y, const.WALL_IMAGE))
         for east_wall in range(const.SCREEN_H, 0, -const.TILE_SIZE):
             x = const.SCREEN_W - const.TILE_SIZE
             y = east_wall
-            Obstacle(x, y, const.WALL_IMAGE).draw(screen)
+            self.obstacles.append(Obstacle(x, y, const.WALL_IMAGE))
         for south_wall in range(const.SCREEN_W, 0, -const.TILE_SIZE):
             x = south_wall
             y = const.SCREEN_H - const.TILE_SIZE
-            Obstacle(x, y, const.WALL_IMAGE).draw(screen)
+            self.obstacles.append(Obstacle(x, y, const.WALL_IMAGE))
         for west_wall in range(0, const.SCREEN_H, const.TILE_SIZE):
             x = 0
             y = west_wall
-            Obstacle(x, y, const.WALL_IMAGE).draw(screen)
+            self.obstacles.append(Obstacle(x, y, const.WALL_IMAGE))
 
     def create_exit(self):
         """Creates an exit direction randomly from N, E, S, W, excluding entrance direction if it exists."""
@@ -45,6 +48,16 @@ class Room:
         if self.entrance is not None:
             valid_directions.remove(self.entrance)
         return random.choice(valid_directions)
+
+    def open_exit(self):
+        # TODO Remove obstacles at direction point
+        pass
+
+    def complete_room(self):
+        """Flags the room as complete and opens the exit"""
+        assert not self.complete, 'Trying to complete an already complete room!'
+        self.complete = True
+        self.open_exit()
 
     def get_player_spawn_point(self):
         """Returns a tuple with the X, Y coordinates of where the player will spawn in the room."""
@@ -54,4 +67,5 @@ class Room:
         return self.PLAYER_SPAWN_POINTS[self.entrance]
 
     def draw(self, screen):
-        self.create_border_wall(screen)
+        for obstacle in self.obstacles:
+            obstacle.draw(screen)
